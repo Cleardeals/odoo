ce8a12d3c96b701d9cbb8f891b8e01d5fd91aa4e
Vivek Shahi <shahivivek503@gmail.com>
2026-06-18 11:13:59 +0530

    DMO-107, DMO-109, DMO-117 Adding Custom Security Group
    Adding custom group as Deal Manger and Data Entry Operator and in ir.model.access.csv adding permission like Read, Write, Update, Delete

--- custom_addons/deals/__manifest__.py
+++ custom_addons/deals/__manifest__.py
@@ -7,14 +7,13 @@
     "description": "Comprehensive deals management system for tracking and managing deal owners, their deal offers, and associated deal packages across properties.",
     "data": [
         # 1. Security (Always First)
+        "security/deals_security.xml",
         "security/ir.model.access.csv",
-        # 2. Menu Views
-        "views/deal_owner_menu.xml",
-        "views/deal_offer_menu.xml",
-        "views/deal_package_menu.xml",
-        # 3. Model Views
+        # 2. Model Views
         "views/deal_owner_views.xml",
         "views/deal_offer_views.xml",
         "views/deal_package_views.xml",
+        # 3. Menu Views
+        "views/deal_menu.xml",
     ],
 }
