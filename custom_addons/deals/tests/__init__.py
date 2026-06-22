fda6a34653cdd6168db2efef4887c3aac800efbe
Vivek Shahi <shahivivek503@gmail.com>
2026-06-22 10:15:56 +0530

    DMO-57 Updating init.py
    Adding test_deal_owner, test_deal_package in init.py of test folder in deal module

--- custom_addons/deals/tests/__init__.py
+++ custom_addons/deals/tests/__init__.py
@@ -1,4 +1,4 @@
-from . import (
-    test_deal_common,
-    test_deal_offer,
-)
+from . import test_deal_common
+from . import test_deal_offer
+from . import test_deal_package
+from . import test_deal_owner
