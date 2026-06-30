a3140b31c717761d4566bed538a69c51bbd8c35f
Vivek Shahi <shahivivek503@gmail.com>
2026-06-30 17:01:03 +0530

    DMO-57 Adding test file for security, transaction, deal

--- custom_addons/deals/tests/__init__.py
+++ custom_addons/deals/tests/__init__.py
@@ -2,3 +2,6 @@ from . import test_deal_common
 from . import test_deal_offer
 from . import test_deal_package
 from . import test_deal_owner
+from . import test_deal_security
+from . import test_deal_transaction
+from . import test_deal 
